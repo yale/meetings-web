c0d04436a958594b116631584cc352a97f07b57f
Yale Spector <yspector@wework.com>
2019-01-16 15:15:22 -0500

    try to fix csrf login bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ENV.fetch('AUTH0_SECRET'),
     ENV.fetch('AUTH0_DOMAIN'),
     callback_path: '/auth/oauth2/callback',
+    provider_ignores_state: true,
     authorize_params: {
       scope: 'openid email profile'
     }
